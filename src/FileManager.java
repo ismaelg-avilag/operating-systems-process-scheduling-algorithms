493f9f5a7d20f82bec785d21691d1229da576865
Ismael Avila <ismaelg.avilag@gmail.com>
2023-09-19 16:36:03 -0600

    File Manager Class
    - Implemented static method "readProccessesFile" that reads the test file and returns a list of processes

---
+++ src/FileManager.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class FileManager {
+
+    public static ArrayList<Process> readProccessesFile(String filePath)
+    {
+        ArrayList<Process> processesList = new ArrayList<Process>();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            String line = reader.readLine();
+
+            while (line != null) {
+                String[] processInfo = line.split(",");
+                processesList.add(new Process(processInfo[0], Integer.parseInt(processInfo[1]), Integer.parseInt(processInfo[2])));
+                line = reader.readLine();
+            }
+
+            reader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return processesList;
+    }
+}
