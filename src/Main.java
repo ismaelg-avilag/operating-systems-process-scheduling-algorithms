97b712bf2c99765e70f8279d7c92122091931f13
Ismael Avila <ismaelg.avilag@gmail.com>
2023-09-19 18:13:56 -0600

    Improvements
    - Added a 3 second sleep to the Round Robin algorithm
    - Screen cleaning removed
    - Changed to the new switch syntax

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws InterruptedException {
         ArrayList<Process> processesList = FileManager.readProccessesFile("processes.txt");
         Scanner scanner = new Scanner(System.in);
 
@@ -21,38 +20,33 @@ public class Main {
             option = scanner.nextInt();
 
             switch (option) {
-                case 1:
+                case 1 -> {
                     System.out.println("Orden de ejecución con el algoritmo FCFS...");
-                    for(String s : PlanningAlgorithms.FCFS(processesList))
+                    for (String s : PlanningAlgorithms.FCFS(processesList))
                         System.out.println(s);
-                break;
-
-                case 2:
+                }
+                case 2 -> {
                     System.out.println("Orden de ejecución con el algoritmo SJF...");
-                    for(String s : PlanningAlgorithms.SJF(processesList))
+                    for (String s : PlanningAlgorithms.SJF(processesList))
                         System.out.println(s);
-                break;
-
-                case 3:
+                }
+                case 3 -> {
                     System.out.println("Orden de ejecución con el algoritmo Prioridades...");
-                    for(String s : PlanningAlgorithms.Priorities(processesList))
+                    for (String s : PlanningAlgorithms.Priorities(processesList))
                         System.out.println(s);
-                break;
-
-                case 4:
+                }
+                case 4 -> {
                     System.out.println("Orden de ejecución con el algoritmo Round Robin...");
-                    for(String s : PlanningAlgorithms.RoundRobin(processesList, 3))
+                    for (String s : PlanningAlgorithms.RoundRobin(processesList, 3)) {
                         System.out.println(s);
-                break;
-
-                case 5:
-                    System.out.println("Saliendo...");
-                break;
+                        Thread.sleep(3000);
+                    }
+                }
+                case 5 -> System.out.println("Saliendo...");
             }
 
             System.out.println("Presiona enter para continuar...");
             scanner.nextLine();
-            try { Runtime.getRuntime().exec("clear"); } catch (IOException e) { throw new RuntimeException(e); }
         }
 
         scanner.close();
