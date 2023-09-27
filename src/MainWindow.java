bd362fb007f4c056d5601e2898e71e7626198e4b
Ismael Avila <ismaelg.avilag@gmail.com>
2023-09-26 19:09:53 -0600

    UI Finished

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,18 +1,18 @@
 import javax.swing.*;
 
 public class MainWindow {
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JRadioButton radioButton1;
-    private JRadioButton radioButton2;
-    private JRadioButton radioButton3;
-    private JRadioButton radioButton4;
-    private JRadioButton radioButton5;
-    private JRadioButton radioButton6;
-    private JSpinner spinner1;
-    private JSpinner spinner2;
-    private JTextField textField1;
-    private JTextArea textArea1;
-    private JTextArea textArea2;
+    private JButton buttonLoadProcesses;
+    private JButton buttonAddProcess;
+    private JButton buttonExecute;
+    private JRadioButton radioButtonBegin;
+    private JRadioButton radioButtonEnd;
+    private JRadioButton radioButtonFCFS;
+    private JRadioButton RadioButtonSJF;
+    private JRadioButton radioButtonPriorities;
+    private JRadioButton radioButtonRoundRobin;
+    private JSpinner spinnerDuration;
+    private JSpinner spinnerPriority;
+    private JTextField textFieldProcessName;
+    private JTextArea textAreaAlgorithOutput;
+    private JTextArea textAreaProcesses;
 }
