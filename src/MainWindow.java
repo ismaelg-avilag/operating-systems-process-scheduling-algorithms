8bd5afb1346268ae6666ee7d0a58651c4264cb23
Ismael Avila <ismaelg.avilag@gmail.com>
2023-09-26 18:43:03 -0600

    User Interface created

---
+++ src/MainWindow.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+
+public class MainWindow {
+    private JPanel panel1;
+    private JTextField textFieldProcessName;
+    private JSpinner spinnerDuration;
+    private JSpinner spinnerPriority;
+    private JButton ButtonAddProcess;
+    private JButton ButtonRunAlgorithm;
+    private JTextArea textAreaExecution;
+    private JTextArea textAreaProcessList;
+    private JRadioButton radioButtonInicio;
+    private JRadioButton radioButtonFinal;
+    private JRadioButton radioButtonFCFS;
+    private JRadioButton radioButtonSJF;
+    private JRadioButton radioButtonPriorities;
+    private JRadioButton radioButtonRoundRobin;
+    private JButton buttonLoadProcess;
+
+}
