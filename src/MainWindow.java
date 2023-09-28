19eaddd79ae5d438eaa8e2180b7e2a102103dca0
Ismael Avila <ismaelg.avilag@gmail.com>
2023-09-28 00:13:25 -0600

    Implementation of window functionalities
    - The constructor of the qMainWindow class was implemented, in which the ButtonGroup is made for the radiobuttons
    - Listeners for buttons were implemented
    - Added functionality for each of the buttons on the graphical interface
    - The main() method was implemented, in which an instance of the window is created

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class MainWindow {
     private JButton buttonLoadProcesses;
@@ -15,4 +16,89 @@ public class MainWindow {
     private JTextField textFieldProcessName;
     private JTextArea textAreaAlgorithOutput;
     private JTextArea textAreaProcesses;
+    private JPanel panelMain;
+
+    ArrayList<Process> processes = new ArrayList<>();
+
+    public MainWindow() {
+        ButtonGroup processOrder = new ButtonGroup();
+        processOrder.add(radioButtonBegin);
+        processOrder.add(radioButtonEnd);
+
+        ButtonGroup algorithmSelection = new ButtonGroup();
+        algorithmSelection.add(radioButtonFCFS);
+        algorithmSelection.add(RadioButtonSJF);
+        algorithmSelection.add(radioButtonPriorities);
+        algorithmSelection.add(radioButtonRoundRobin);
+
+        buttonLoadProcesses.addActionListener(e -> {
+            FileManager.readProccessesFile("processes.txt", processes);
+
+            for (Process process : processes)
+                textAreaProcesses.append(process.toString() + "\n");
+
+            buttonLoadProcesses.setEnabled(false);
+        });
+
+        buttonAddProcess.addActionListener(e -> {
+            String processName = textFieldProcessName.getText();
+            int duration = (int) spinnerDuration.getValue();
+            int priority = (int) spinnerPriority.getValue();
+
+            Process p = new Process(processName, duration, priority);
+
+            if(radioButtonBegin.isSelected())
+                processes.add(0, p);
+            else if(radioButtonEnd.isSelected())
+                processes.add(p);
+
+            refreshProcessesTextArea();
+            inputReset();
+        });
+
+        buttonExecute.addActionListener(e -> {
+            textAreaAlgorithOutput.setText("");
+
+            if(radioButtonFCFS.isSelected())
+                for (String s : PlanningAlgorithms.FCFS(processes))
+                    textAreaAlgorithOutput.append(s + "\n");
+
+            else if(RadioButtonSJF.isSelected())
+                for (String s : PlanningAlgorithms.SJF(processes))
+                    textAreaAlgorithOutput.append(s + "\n");
+
+            else if(radioButtonPriorities.isSelected())
+                for (String s : PlanningAlgorithms.Priorities(processes))
+                    textAreaAlgorithOutput.append(s + "\n");
+
+            else if(radioButtonRoundRobin.isSelected())
+                for (String s : PlanningAlgorithms.RoundRobin(processes, 3))
+                    textAreaAlgorithOutput.append(s + "\n");
+
+        });
+    }
+
+    private void refreshProcessesTextArea()
+    {
+        textAreaProcesses.setText("");
+
+        for (Process process : processes)
+            textAreaProcesses.append(process.toString() + "\n");
+    }
+
+    private void inputReset() {
+        textFieldProcessName.setText("");
+        spinnerDuration.setValue(0);
+        spinnerPriority.setValue(0);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Algoritmos de Planificación de Procesos");
+        frame.setContentPane(new MainWindow().panelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
 }
